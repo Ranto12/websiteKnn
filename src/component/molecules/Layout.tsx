9af30e710909ce52b6cbd44a33a6e2fa5c3427a6
ranto <rantoirwanda08@gmail.com>
2024-01-26 20:48:57 +0700

    add navbar

--- src/component/molecules/Layout.tsx
+++ src/component/molecules/Layout.tsx
@@ -1,10 +1,31 @@
+import { Flex, Tabs, Text } from "@mantine/core";
+import { NavLink } from "react-router-dom";
+
 const Layout = () => {
   return (
-    <div>
-        <div>Layout</div>
-        <div>contentasad</div>
-    </div>
-  )
-}
+    <>
+      <Flex justify="center">
+        <Tabs defaultValue="gallery" onClick={(e) => nav}>
+          <Tabs.List>
+            <NavLink to={"/"}>
+              <Tabs.Tab value="gallery">
+                <Text size="xl" className="font-medium">
+                  Rekomendasi
+                </Text>
+              </Tabs.Tab>
+            </NavLink>
+            <NavLink to={"/data"}>
+              <Tabs.Tab value="messages">
+                <Text size="xl" className="font-medium">
+                  Data
+                </Text>
+              </Tabs.Tab>
+            </NavLink>
+          </Tabs.List>
+        </Tabs>
+      </Flex>
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
